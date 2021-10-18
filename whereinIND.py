7991dbf5d470f12eaa4ded411737adbf4f05e2ee
Manmohansinh <85485208+Manmohansinh@users.noreply.github.com>
2021-10-18 16:31:30 +0530

    Add files via upload

---
+++ whereinIND.py
@@ -0,0 +1,15 @@
+import mysql.connector as SQLC
+DataBase = SQLC.connect(
+    host="localhost",
+    user="root",
+    password="manmohan65",
+    port="3306",
+    database="Hospital"
+)
+
+Cursor = DataBase.cursor ( )
+sql = "SELECT * FROM patient WHERE Country = 'IND'"
+Cursor.execute(sql)
+myresult =Cursor.fetchall ( )
+for x in myresult :
+  print(x)
